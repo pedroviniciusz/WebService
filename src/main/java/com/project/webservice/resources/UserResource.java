d9cb9314b165a9c055cef4f6de233e175c0b0694
Pedro Vinicius <pedrovdjesus@hotmail.com>
2022-01-20 19:54:44 -0300

    Criada Entity

---
+++ src/main/java/com/project/webservice/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.project.webservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.webservice.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "pedro", "pedro@teste", "777", "1234");
+		return ResponseEntity.ok().body(user);
+	}
+	
+}

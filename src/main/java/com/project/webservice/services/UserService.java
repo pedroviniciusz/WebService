6228791b45245981999f745e203015959b6079f8
Pedro Vinicius <pedrovdjesus@hotmail.com>
2022-03-07 21:12:26 -0300

    Tratamento de exceções

--- src/main/java/com/project/webservice/services/UserService.java
+++ src/main/java/com/project/webservice/services/UserService.java
@@ -3,39 +3,57 @@ package com.project.webservice.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.project.webservice.entities.User;
 import com.project.webservice.repositories.UserRepository;
+import com.project.webservice.services.exceptions.DatabaseException;
+import com.project.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-		Optional <User> obj = repository.findById(id);
-		return obj.get();
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException er) {
+			throw new DatabaseException(er.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
